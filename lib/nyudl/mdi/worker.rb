2b4fa6b65f6f2f047fde05ba44509d526c40c005
jgpawletko <jgpawletko@gmail.com>
2015-02-22 22:50:29 -0500

    initial commit

---
+++ lib/nyudl/mdi/worker.rb
@@ -0,0 +1,9 @@
+require "nyudl/mdi/worker/version"
+
+module Nyudl
+  module Mdi
+    module Worker
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/nyudl/mdi/worker/version.rb
@@ -0,0 +1,7 @@
+module Nyudl
+  module Mdi
+    module Worker
+      VERSION = "0.1.0"
+    end
+  end
+end
